2160ba957654526761a84979d3028148a901f9a3
YasinCoban123 <9018681@student.zadkine.nl>
2023-02-02 12:42:02 +0100

    yccc

--- html en php opdracht/action_page.php
+++ html en php opdracht/action_page.php
@@ -1,5 +1,9 @@
 <?php
+ echo date("") . "<br>";
+ echo date("l jS \of F Y h:i:s A") . "<br>";
 echo "<p>hello world</p> <br>";
-
-echo 
+$username = $_POST['username'];
+$password = $_POST['password'];
+echo "Username: $username<br>";
+echo "Password: $password";
 ?>
