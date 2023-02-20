ad0d94fa85aa042cbaba07dfb2e6edffbd9a009b
YasinCoban123 <9018681@student.zadkine.nl>
2023-02-20 16:00:08 +0100

    chgc

--- html en php opdracht/functions.php
+++ html en php opdracht/functions.php
@@ -12,25 +12,27 @@ function OpenDb() {
 }
 
 function OvzBieren() {
-  $conn = OpenDb();
-  echo "<table border='1'>";
-  echo "<tr><th>id</th><th>bieren</th><th>Alcoholpercentage</th></tr>";
-
-  try {
-      $stmt = $conn->prepare("SELECT * FROM bier"); 
-      $stmt->execute();
-
-      while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-          echo "<tr>";
-          echo "<td>" . $row["biercode"] . "</td>";
-          echo "<td>" . $row["naam"] . "</td>";
-          echo "<td>" . $row["alcohol"] . "</td>";
-          echo "</tr>";
-      }
-
-  } catch (PDOException $e) {
-      echo "Error: " . $e->getMessage();
+    $conn = OpenDb();
+    echo "<table border='1'>";
+    echo "<tr><th>id</th><th>bieren</th><th>Alcoholpercentage</th></tr>";
+  
+    try {
+        $stmt = $conn->prepare("SELECT * FROM bier"); 
+        $stmt->execute();
+  
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($rows as $row) {
+            echo "<tr>";
+            echo "<td>" . $row["biercode"] . "</td>";
+            echo "<td>" . $row["naam"] . "</td>";
+            echo "<td>" . $row["alcohol"] . "</td>";
+            echo "</tr>";
+        }
+  
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    echo "</table>";
+    $conn = null;
   }
-  echo "</table>";
-  $conn = null;
-}
+  
--- html en php opdracht/overzicht_bieren.php
+++ html en php opdracht/overzicht_bieren.php
@@ -11,6 +11,7 @@
 // print bieren
         OvzBieren();
         
+        OvzBrouwers();
         ?>
     </body>
 </html>
